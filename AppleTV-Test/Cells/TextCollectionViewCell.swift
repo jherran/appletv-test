8a801b1248f1a882c4823805d17ae8d2fa8f6bfb
Jose A. Herran <jaherran@atsistemas.com>
2019-04-26 09:54:25 +0200

    Fixed focus issues

---
+++ AppleTV-Test/Cells/TextCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TextCollectionViewCell.swift
+//  AppleTV-Test
+//
+//  Created by José Ángel Herrán on 11/04/2019.
+//  Copyright © 2019 Jose Angel Herran. All rights reserved.
+//
+
+import UIKit
+
+class TextCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellTitle: UILabel!
+}
